76df6a5313b5973b4e53502036feaddef716cb57
salmanahmed643 <647salman@gmail.com>
2021-12-18 22:29:17 +0600

    signup system added

--- src/component/Login/SignUp/SignUp.js
+++ src/component/Login/SignUp/SignUp.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        setError(false)
+        try{
+            const res = await axios.post("http://localhost:7000/api/auth/signup", {
+                username,
+                email,
+                password
+            });
+            res.data && window.location.replace("/signin")
+        } catch(err) {
+            setError(true)
+        }
+
+    };
+
     return (
         <>
             <div className="SignInSection">
                 <div className="signInSignUpTitle">
                     <h3>SignUp</h3>
                 </div>
-                <form className="signInForm">
+                <form onSubmit={handleSignUp} className="signInForm">
                     <label htmlFor="username">Username</label>
                     <input 
                         type="text" 
                         placeholder="Username" 
                         id="username"
                         className="signInInput"
+                        onBlur={e => setUsername(e.target.value)}
                     />
                     <label htmlFor="email">Email</label>
                     <input 
@@ -22,6 +45,7 @@ const SignUp = () => {
                         placeholder="Email" 
                         id="email"
                         className="signInInput"
+                        onBlur={e => setEmail(e.target.value)}
                     />
                     <label htmlFor="password">Password</label>
                     <input 
@@ -29,13 +53,24 @@ const SignUp = () => {
                         placeholder="Password" 
                         id="password" 
                         className="signInInput"
+                        onBlur={e => setPassword(e.target.value)}
                     />
-                    <button className="signInButton"><a href="/">Sign Up</a></button>
+                    <button
+                        type="submit"
+                        className="signInButton">
+                        Sign Up
+                    </button>
                 </form>
                 <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <span className="dontAccount">Already account?</span>
                     <button className="signInSignUpButton"> <Link to="/signin">Sign In</Link></button>
                 </div>
+                {
+                    error &&
+                    <span
+                        style={{backgroundColor: "red", padding: '.5rem 1rem', borderRadius: ".5rem", color: "#fff", fontSize: "1.2rem", marginTop: ".5rem"}}
+                    >something wrong</span>
+                }
             </div>
         </>
     );
